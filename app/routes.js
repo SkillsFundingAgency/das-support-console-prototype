5b7c42540ef59403d36963028135da0225021e4d
briananddigital <86655162+BrianMilman@users.noreply.github.com>
2021-08-05 16:12:46 +0100

    start of journey

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,37 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+
+
+
+
+router.post(`/350/initial-search`, function (req, res) {
+    const userORaccount = req.session.data['initial-search'];
+    
+
+    if (userORaccount === 'users'
+       ) {
+        res.redirect(`/350/user-list`);
+    }
+   else res.redirect(`/350/account-list`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router
