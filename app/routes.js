c8e4bb8699d04396b05f11527ba2a53207bb56d0
briananddigital <86655162+BrianMilman@users.noreply.github.com>
2021-08-10 14:01:22 +0100

    Creation of as-is journeys

--- app/routes.js
+++ app/routes.js
@@ -6,38 +6,63 @@ const router = express.Router()
 
 
 
-
-router.post(`/350/initial-search`, function (req, res) {
+// TIER 1 STARTS
+router.post(`/350/tier1/initial-search`, function (req, res) {
     const userORaccount = req.session.data['initial-search'];
     
 
     if (userORaccount === 'users'
        ) {
-        res.redirect(`/350/user-list`);
+        res.redirect(`/350/tier1/user-list`);
     }
-   else res.redirect(`/350/account-list`);
+   else res.redirect(`/350/tier1/account-list`);
 });
 
 
 
 
-router.post(`/350/uln-or-cohort`, function (req, res) {
+router.post(`/350/tier1/uln-or-cohort`, function (req, res) {
     const ulnORcohort = req.session.data['uln-or-cohort'];
     
 
     if (ulnORcohort === 'uln'
        ) {
-        res.redirect(`/350/uln-view`);
+        res.redirect(`/350/tier1/uln-view`);
     }
-   else res.redirect(`/350/cohort-summary`);
+   else res.redirect(`/350/tier1/cohort-summary`);
+});
+
+// TIER 1 ENDS
+
+
+// TIER 2 STARTS
+
+router.post(`/350/tier2/initial-search`, function (req, res) {
+    const userORaccount = req.session.data['initial-search'];
+    
+
+    if (userORaccount === 'users'
+       ) {
+        res.redirect(`/350/tier2/user-list`);
+    }
+   else res.redirect(`/350/tier2/account-list`);
 });
 
 
 
 
+router.post(`/350/tier2/uln-or-cohort`, function (req, res) {
+    const ulnORcohort = req.session.data['uln-or-cohort'];
+    
 
+    if (ulnORcohort === 'uln'
+       ) {
+        res.redirect(`/350/tier2/uln-view`);
+    }
+   else res.redirect(`/350/tier2/cohort-summary`);
+});
 
-
+// TIER 2 ENDS
 
 
 
