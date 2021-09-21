7e43a892d9aa81375b21e3ebed59c8e2a7b9670f
briananddigital <90381147+BrianMilmanXansium@users.noreply.github.com>
2021-09-21 10:11:30 +0100

    updated support console

--- app/routes.js
+++ app/routes.js
@@ -73,6 +73,49 @@ router.post('/356/address-details-correct', (req, res) => {
 })
     
 
+// TO BE DESIGNS START HERE
+
+
+
+
+// TIER 2 STARTS
+
+router.post(`/350/to-be/tier2/initial-search`, function (req, res) {
+    const userORaccount = req.session.data['initial-search'];
+    
+
+    if (userORaccount === 'users'
+       ) {
+        res.redirect(`/350/to-be/tier2/user-list`);
+    }
+   else res.redirect(`/350/to-be/tier2/account-list`);
+});
+
+
+
+
+router.post(`/350/to-be/tier2/uln-or-cohort`, function (req, res) {
+    const ulnORcohort = req.session.data['uln-or-cohort'];
+    
+
+    if (ulnORcohort === 'uln'
+       ) {
+        res.redirect(`/350/to-be/tier2/uln-view`);
+    }
+   else res.redirect(`/350/to-be/tier2/cohort-summary`);
+});
+
+// TIER 2 ENDS
+
+router.post('/356/to-be/address-details-correct', (req, res) => {
+	if(req.session.data['address-details-correct'] == 'Yes'){
+		res.redirect('check-and-submit')
+	} else {
+		res.redirect('add-address')
+	}
+})
+    
+
 
 
 
